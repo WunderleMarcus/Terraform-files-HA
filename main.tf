0e1b2d1bba9a0b94389f5780ff0b25869d0b7fab
Marcus Wunderle <102217370+WunderleMarcus@users.noreply.github.com>
2022-10-26 16:17:01 +0200

    Add files via upload
    Homework from Oct. 26th... The first three exercises.

---
+++ main.tf
@@ -0,0 +1,36 @@
+# deklaration der ressourcen gruppe und location
+locals {
+ Ressource_Group_Name = "marcus-wunderle-rg"
+ Ressource_Group_Location = "West Europe"
+}
+
+
+## resourcen gruppe erstellen
+resource "azurerm_resource_group" "marcus-wunderle-rg" {
+  name     = local.Ressource_Group_Name
+  location = local.Ressource_Group_Location
+}
+
+
+## erstellen des storage accounts
+resource "azurerm_storage_account" "demo_account_marcus" {
+  name                     = "demostoragemarcus"
+  resource_group_name      = local.Ressource_Group_Name
+  location                 = local.Ressource_Group_Location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  depends_on = [
+    azurerm_resource_group.marcus-wunderle-rg
+  ]
+}
+
+# erstellen des blob containers
+resource "azurerm_storage_container" "tsblobcontainer" {
+  name                  = "tsblobcontainer"
+  storage_account_name  = "demostoragemarcus"
+  container_access_type = "blob"
+  depends_on = [
+    azurerm_storage_account.demo_account_marcus
+  ]
+}
---
+++ variable.tfvars
@@ -0,0 +1,15 @@
+variable "name" {
+    type = string
+    description = "Beschreibung des Namens der RessourcenGruppen"
+
+    default = "marcus_wunderle_rg"
+    name = "marcus_wunderle_rg"
+}
+
+variable "location" {
+    type = string
+    description = "(optional) describe your variable"
+
+    default = "West Europe"
+    name = "West Europe"
+}
